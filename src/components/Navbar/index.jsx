17a4f1cd6e1eeadcd85bbd2909fb3b070736c45e
Deivisson Henrique <profhdeivisson@yahoo.com>
2023-05-25 21:52:29 -0300

    Alteração Título Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -71,7 +71,7 @@ export default function Navbar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            Pokedex | React
           </Typography>
           <Search>
             <SearchIconWrapper>

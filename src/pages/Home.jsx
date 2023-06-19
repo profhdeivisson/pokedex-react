2c3e7b175336845508699443949dfac6fd2babaa
Deivisson Henrique <profhdeivisson@yahoo.com>
2023-06-19 12:37:18 -0300

    Filtro atualizado. Agora, a filtragem ocorre também conforme as letras vão sendo apagadas.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,49 +6,56 @@ import Navbar from '../components/Navbar'
 import PokemonCard from '../components/PokemonCard'
 
 export const Home = () => {
-    const [pokemons, setPokemons] = useState([])
+    const [pokemons, setPokemons] = useState([]);
+    const [filteredPokemons, setFilteredPokemons] = useState([]);
+  
     useEffect(() => {
-        getPokemons();
+      getPokemons();
     }, []);
+  
     const getPokemons = () => {
-        var endpoints = [];
-        for (let i = 1; i <= 50; i++) {
-            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        }
-        console.log(endpoints)
-        var response = axios.all(endpoints.map(endpoint => axios.get(endpoint))).then((response) => setPokemons(response));
-        return response;
-        // axios.get("https://pokeapi.co/api/v2/pokemon?limit=50").then((response) => setPokemons(response.data.results)).catch((error) => console.error(error));
-    }
-
+      var endpoints = [];
+      for (let i = 1; i <= 50; i++) {
+        endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      }
+      axios
+        .all(endpoints.map(endpoint => axios.get(endpoint)))
+        .then(response => {
+          setPokemons(response);
+          setFilteredPokemons(response);
+        })
+        .catch(error => console.error(error));
+    };
+  
     const pokemonFilter = (name) => {
-        var filteredPokemons = []
-        if(!name){
-            getPokemons()
-        }
-        for(var i in pokemons){
-            if(pokemons[i].data.name.includes(name)){
-                filteredPokemons.push(pokemons[i])
-            }
-        }
-        // console.log(filteredPokemons)
-        setPokemons(filteredPokemons)
-    }
-    
-    return(
-        <div>
-            <Navbar pokemonFilter={pokemonFilter}/>
-            <Container maxWidth="false">
-                <Grid container spacing={1}>
-                {pokemons.map((pokemon, key) => {
-                    return (
-                        <Grid item xs={6} sm={3} md={2} key={key}> 
-                        <PokemonCard id={pokemon.data.id} name={pokemon.data.name} image={pokemon.data.sprites.front_default}/>
-                        </Grid>
-                    );
-                    })}
+      if (!name) {
+        setFilteredPokemons(pokemons);
+      } else {
+        const filteredPokemons = pokemons.filter(
+          (pokemon) => pokemon.data.name.includes(name)
+        );
+        setFilteredPokemons(filteredPokemons);
+      }
+    };
+  
+    return (
+      <div>
+        <Navbar pokemonFilter={pokemonFilter} />
+        <Container maxWidth="false">
+          <Grid container spacing={1}>
+            {filteredPokemons.map((pokemon, key) => {
+              return (
+                <Grid item xs={6} sm={3} md={2} key={key}>
+                  <PokemonCard
+                    id={pokemon.data.id}
+                    name={pokemon.data.name}
+                    image={pokemon.data.sprites.front_default}
+                  />
                 </Grid>
-            </Container>
-        </div>
-    )
-}
+              );
+            })}
+          </Grid>
+        </Container>
+      </div>
+    );
+  };  
